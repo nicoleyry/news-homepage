617919a91079f2340d81d2370e5745588bf6ebbb
Nicole Yang <nicoleyry@outlook.com>
2023-09-08 16:48:17 +0800

    add attribution

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
 		<div className='App'>
 			<Navbar />
 			<Content />
+			<div className="attribution">
+				<span>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. </span>
+				<span>Coded by <a href="https://www.nicoleyry.com/">Nicole Yang</a>. </span>
+			</div>
 		</div>
 	);
 }
